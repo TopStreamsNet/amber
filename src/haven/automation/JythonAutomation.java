d3da38890f4a15e0f642f031b9d0ca48d7fce549
0wnag3 <0wnag3@nomail.none>
2017-02-14 07:15:36 -0500

    Adding DreamHarvester

--- src/haven/automation/JythonAutomation.java
+++ src/haven/automation/JythonAutomation.java
@@ -14,50 +14,61 @@ import java.io.FileNotFoundException;
 
 
 public class JythonAutomation implements Runnable {
-	private String botname = "";
-	private Boolean terminate = false;
+    private String botname = "";
+    private Boolean run = false;
+    private Boolean terminate = false;
 
-	public Boolean getTerminate() {
-		return terminate;
-	}
+    public Boolean getRun() {
+        return run;
+    }
 
-	public void setTerminate(Boolean terminate) {
-		this.terminate = terminate;
-	}
+    public void setRun(Boolean run) {
+        this.run = run;
+    }
 
-	public static class JythonAutomationHolder {
-		public static final JythonAutomation HOLDER_INSTANCE = new JythonAutomation();
-	}
-	public static JythonAutomation getInstance(){
-		return JythonAutomationHolder.HOLDER_INSTANCE;
-	}
-		
-	public JythonAutomation() {
-		// TODO Auto-generated constructor stub
-	}
 
-	@Override
-	public void run() {
-		// TODO Auto-generated method stub
-		HavenPanel.lui.cons.out.println("--Running Jython Automation: "+this.botname+"--");
-		PythonInterpreter interp = new PythonInterpreter();
-		try {
-			FileInputStream fis = new FileInputStream(new File("jbot/"+this.botname));
-			interp.execfile(fis);
-		} catch (Exception e) {
-			HavenPanel.lui.cons.out.println("Something went wrong!");
-			e.printStackTrace(HavenPanel.lui.cons.out);
-		}
-		HavenPanel.lui.cons.out.println("--/Running Jython Automation--");
-		this.terminate=false;
-		
+    public Boolean getTerminate() {
+        return terminate;
+    }
 
-	}
+    public void setTerminate(Boolean terminate) {
+        this.terminate = terminate;
+    }
 
-	public void start(String[] args) {
-		this.botname = args[1];
-		Thread jt = new Thread(JythonAutomation.getInstance(),"JBot thread");
-		jt.start();
-	}
+    public static class JythonAutomationHolder {
+        public static final JythonAutomation HOLDER_INSTANCE = new JythonAutomation();
+    }
+
+    public static JythonAutomation getInstance() {
+        return JythonAutomationHolder.HOLDER_INSTANCE;
+    }
+
+    public JythonAutomation() {
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public void run() {
+        // TODO Auto-generated method stub
+        HavenPanel.lui.cons.out.println("--Running Jython Automation: " + this.botname + "--");
+        PythonInterpreter interp = new PythonInterpreter();
+        try {
+            FileInputStream fis = new FileInputStream(new File("jbot/" + this.botname));
+            interp.execfile(fis);
+        } catch (Exception e) {
+            HavenPanel.lui.cons.out.println("Something went wrong!");
+            e.printStackTrace(HavenPanel.lui.cons.out);
+        }
+        HavenPanel.lui.cons.out.println("--/Running Jython Automation--");
+        this.terminate = false;
+
+
+    }
+
+    public void start(String[] args) {
+        this.botname = args[1];
+        Thread jt = new Thread(JythonAutomation.getInstance(), "JBot thread");
+        jt.start();
+    }
 
 }
