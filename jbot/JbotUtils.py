40baa6637df177e86ca8f70c5ffc578103bd351f
0wnag3 <0wnag3@topstreams.net>
2017-03-02 16:28:53 -0500

    Farmer bot

--- jbot/JbotUtils.py
+++ jbot/JbotUtils.py
@@ -7,7 +7,7 @@ from synchronize import make_synchronized
 from time import sleep
 
 @make_synchronized
-def gettilegobs(oc,coord,gui):
+def gettilegobs(oc,coord):
     gobs = []
     for gob in oc:
         res = gob.getres()
@@ -15,6 +15,35 @@ def gettilegobs(oc,coord,gui):
         if res != None and dist < 1:
             gobs.append(gob)
     return gobs
+
+def getfreeslot(item, inventory):
+    gui = HavenPanel.lui.root.getchild(GameUI)
+    tilesize = 33
+    HavenPanel.lui.cons.out.println("!!!item {0} inventory {1}".format(item, inventory))
+    invitems = inventory.getItemsPartial("")
+    if len(invitems) == 0:
+        return Coord.z
+    for y in range(1, inventory.sz.y, tilesize):
+        for x in range(1, inventory.sz.x, tilesize):
+            if (x+item.sz.x > inventory.sz.x or y+item.sz.y > inventory.sz.y):
+                continue
+            desired_topleft = Coord(x,y)
+            desired_bottomright = Coord(x+item.sz.x,y+item.sz.y)
+            HavenPanel.lui.cons.out.println("- {0}x{1}".format(desired_topleft,desired_bottomright))
+            intersects = False
+            for invitem in invitems:
+                occupied_topleft = invitem.c
+                occupied_bottomright = Coord(invitem.c.x+invitem.sz.x,invitem.c.y+invitem.sz.y)
+                # check if we found an item that intersects
+                if (desired_topleft.x < occupied_bottomright.x and desired_bottomright.x > occupied_topleft.x and
+                    desired_topleft.y < occupied_bottomright.y and desired_bottomright.y > occupied_topleft.y):
+                    intersects = True
+                    break
+            if intersects == False:
+                return Coord(x,y)
+    return None
+
+
 
 def takeitem(item, timeout):
     gui = HavenPanel.lui.root.getchild(GameUI)
@@ -28,12 +57,12 @@ def takeitem(item, timeout):
             return False
     return True
 
-def zloezlo(coord, waitforemptyhand = False, timeout=-1, fallbackcoord=None):
+def itemactcoord(coord, waitforemptyhand=False, timeout=-1, fallbackcoord=None):
     gui = HavenPanel.lui.root.getchild(GameUI)
     gui.map.wdgmsg("itemact", Coord.z, coord.floor(OCache.posres), 0)
     if (waitforemptyhand and not(gui.hand.isEmpty())):
         tout = 0
-        while(not(self.gui.hand.isEmpty())):
+        while(not(gui.hand.isEmpty())):
             HavenPanel.lui.cons.out.println("Waiting for EMPTY hand!")
             sleep(1)
             tout += 1
